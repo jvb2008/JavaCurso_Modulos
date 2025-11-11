3bd11a05a5813eb2975a35b19e607ddbffa6a15c
Josu Motoso <josulanbide@arriagainfo.local>
2025-11-11 19:55:16 +0100

    First commit

--- src/m01_HelloWorld/HelloWorldExercises.java
+++ src/m01_HelloWorld/HelloWorldExercises.java
@@ -17,7 +17,7 @@ public class HelloWorldExercises {
 
 
         // 6. Explora los diferentes System.XXX.println(); más allá de "out".
-//prueba
+//prueba2
 
 
         // 7. Utiliza varios println para imprimir una frase.
