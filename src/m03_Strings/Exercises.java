4443ba37b04c567215933fcdc369643ed7cdf0c3
Josu Motoso <josulanbide@arriagainfo.local>
2025-11-12 21:23:07 +0100

    Added strings 2

--- src/m03_Strings/Exercises.java
+++ src/m03_Strings/Exercises.java
@@ -3,24 +3,36 @@ package m03_Strings;
 public class Exercises {
     static void main() {
         String name = "Josu";
+        String name2 = "josu";
+        String name3 = new String("Josu");
         var surname = new String("Motoso");
 
-        System.out.println(name);
-        System.out.println(surname);
+        //equals
+//        System.out.println(name.equals(name2));
+//        System.out.println(name.equalsIgnoreCase(name2));
+        System.out.println(name.equals(name3));
 
-        //length
-        System.out.println(name.length());
-
-        //obtener un caracter
-        System.out.println(name.charAt(0));
-
-        //Subcadenas
-        System.out.println(name.substring(2));
-        System.out.println(name.substring(2, 4));
-
-        //Mayusculas y minusculas
-        System.out.println(name.toUpperCase());
-        System.out.println(name.toLowerCase());
+//        System.out.println(name);
+//        System.out.println(surname);
+//
+//        //length
+//        System.out.println(name.length());
+//
+//        //obtener un caracter
+//        System.out.println(name.charAt(0));
+//
+//        //Subcadenas
+//        System.out.println(name.substring(2));
+//        System.out.println(name.substring(2, 4));
+//
+//        //Mayusculas y minusculas
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+//
+//        //COntains
+//        String world = "Hola Mundo";
+//        System.out.println(world.contains("hola"));
+//        System.out.println(world.toLowerCase().contains("hola"));
 
 
     }
