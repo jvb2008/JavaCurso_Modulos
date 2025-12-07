50e3fcc55001374f7d207860755e07fa5357a803
jvb2008 <jmotoso001@gmail.com>
2025-12-07 18:59:00 +0100

    Added Java shop project

--- src/m09_Exceptions/Exceptions.java
+++ src/m09_Exceptions/Exceptions.java
@@ -1,23 +1,38 @@
 package m09_Exceptions;
 
 public class Exceptions {
+    static void main() {
+        int h = 2;
+        switch (h) {
+            case 1:
+                System.out.println("A");
+            case 2:
+                System.out.println("B");
+            case 3:
+                System.out.println("C");
+                break;
+            default:
+                System.out.println("D");
+
+        }
+    }
     // 1. Divide dos números almacenados en dos variables. Maneja la división por cero con try-catch.
 
-        // 2. Crea un array de 3 elementos e intenta acceder al índice 5. Captura el ArrayIndexOutOfBoundsException.
+    // 2. Crea un array de 3 elementos e intenta acceder al índice 5. Captura el ArrayIndexOutOfBoundsException.
 
-        // 3. Crea una variable String nula e intenta imprimir su longitud. Maneja el NullPointerException.
+    // 3. Crea una variable String nula e intenta imprimir su longitud. Maneja el NullPointerException.
 
-        // 4. Escribe una función que transforma texto a número. Usa try-catch para manejar entradas no válidas (NumberFormatException).
+    // 4. Escribe una función que transforma texto a número. Usa try-catch para manejar entradas no válidas (NumberFormatException).
 
-        // 5. Escribe un programa con un bloque finally que se ejecute siempre, haya o no error.
+    // 5. Escribe un programa con un bloque finally que se ejecute siempre, haya o no error.
 
-        // 6. Usa throw para lanzar un IllegalArgumentException si un número introducido es negativo.
+    // 6. Usa throw para lanzar un IllegalArgumentException si un número introducido es negativo.
 
-        // 7. Crea una clase TemperatureChecker que lanza una excepción personalizada si la temperatura es menor a -50 o mayor a 50.
+    // 7. Crea una clase TemperatureChecker que lanza una excepción personalizada si la temperatura es menor a -50 o mayor a 50.
 
-        // 8. Crea un programa con varios bloques catch: uno para ArithmeticException, otro para ArrayIndexOutOfBoundsException.
+    // 8. Crea un programa con varios bloques catch: uno para ArithmeticException, otro para ArrayIndexOutOfBoundsException.
 
-        // 9. Crea una función checkPassword(String pass) que lance una excepción si la contraseña es demasiado corta.
+    // 9. Crea una función checkPassword(String pass) que lance una excepción si la contraseña es demasiado corta.
 
-        // 10. Implementa una clase LoginSystem que use una excepción personalizada LoginFailedException si el usuario o contraseña son incorrectos.
+    // 10. Implementa una clase LoginSystem que use una excepción personalizada LoginFailedException si el usuario o contraseña son incorrectos.
 }
